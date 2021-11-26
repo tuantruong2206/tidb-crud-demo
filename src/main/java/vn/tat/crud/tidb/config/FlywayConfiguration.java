f8213427da81a765f546263698847a21933d5e73
Brian Truong <tuantruong2206@gmail.com>
2021-11-26 10:57:49 +0700

    fix issue flyway with tidb

--- src/main/java/vn/tat/crud/tidb/config/FlywayConfiguration.java
+++ src/main/java/vn/tat/crud/tidb/config/FlywayConfiguration.java
@@ -1,6 +1,6 @@
 package vn.tat.crud.tidb.config;
 
-//import org.flywaydb.core.Flyway;
+import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,12 +12,12 @@ import javax.sql.DataSource;
  * Class INFO
  * @date 11/23/21 11:38
  */
-//@Configuration
+@Configuration
 public class FlywayConfiguration {
-//    @Autowired
-//    public FlywayConfiguration(DataSource dataSource) {
-//        Flyway flyway = Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).outOfOrder(true).load();
-//        flyway.repair();
-//        flyway.migrate();
-//    }
+    @Autowired
+    public FlywayConfiguration(DataSource dataSource) {
+        Flyway flyway = Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).outOfOrder(true).load();
+        flyway.repair();
+        flyway.migrate();
+    }
 }
